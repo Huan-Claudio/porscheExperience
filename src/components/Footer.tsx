d89164a95393455dbca94a4e8881524f993e64db
Gabriel-O-Nascimento <gabrielnacimento1011@gmail.com>
2026-04-19 11:22:25 -0300

    Atualização do Footer, adição da data e disciplina.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,13 @@ window.Footer = function Footer({ onNavegar }: IFooterProps) {
 
             // Credits
             React.createElement('div', { className: 'col-md-4 text-center text-md-end' },
-              React.createElement('small', null, 'Projeto acadêmico', React.createElement('br'), 'Huan Cláudio Souza Viana', React.createElement('br'), 'Gabriel de Oliveira Nascimento')
+              React.createElement('small', null, 'Projeto acadêmico', 
+                React.createElement('br'), 
+                'Huan Cláudio Souza Viana', 
+                React.createElement('br'), 
+                'Gabriel de Oliveira Nascimento',
+                React.createElement('br'),
+                '04/2026 - Desenvolvimento de Software Web')
             )
           ),
 
@@ -45,7 +51,7 @@ window.Footer = function Footer({ onNavegar }: IFooterProps) {
           React.createElement('p', {
             className: 'text-center mb-0',
             style: { fontSize: '12px', color: '#333' }
-          }, '© 2025 Porsche Experience. Projeto acadêmico sem fins comerciais.')
+          }, '© 2026 Porsche Experience. Projeto acadêmico sem fins comerciais.')
         )
       )
     )
